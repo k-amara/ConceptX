a7ac54b59c38fd895a45a9d6ab973755cc365915
kamara <22amara.kenza@gmail.com>
2025-03-13 17:59:32 +0100

    explain

--- src/explain.py
+++ src/explain.py
@@ -1,10 +1,31 @@
 import os
 from explainers import *
 from utils import load_vectorizer
+import os
+import pickle as pkl
+
+from model import Model
+from explainers import *
+from utils import arg_parse,load_data, load_vectorizer, save_pkl_path
+from accelerate.utils import set_seed
 
 
-def compute_explanations(instructions, model, vectorizer, args):
-    #### Explain the model ####
+
+def compute_explanations(args, save=True):
+    
+    if args.seed is not None:
+        set_seed(args.seed)
+        
+    model = Model(args)
+    vectorizer = load_vectorizer(args.vectorizer)
+    
+    df = load_data(args)
+    print(df.head())
+    instructions = df['instruction'].tolist()[:3]
+    ids = df["id"].tolist()[:3]
+    
+    vectorizer = load_vectorizer(args.vectorizer)
+    
     # Choose appropriate explainer based on specified explainer
     if args.explainer == "random":
         splitter = TokenizerSplitter()
@@ -17,25 +38,33 @@ def compute_explanations(instructions, model, vectorizer, args):
         explainer = FeatAblation(model, splitter)
     elif args.explainer == "tokenshap":
         splitter = StringSplitter()
-        vectorizer = load_vectorizer("tfidf")
-        explainer = TokenSHAP(model, splitter, vectorizer, debug=True)
+        explainer = TokenSHAP(model, splitter, vectorizer, debug=False, sampling_ratio=1.0)
     elif args.explainer == "conceptshap":
         splitter = ConceptSplitter()
-        vectorizer = load_vectorizer("tfidf")
-        explainer = ConceptSHAP(model, splitter, vectorizer, debug=True)
+        explainer = ConceptSHAP(model, splitter, vectorizer, debug=False, sampling_ratio=1.0)
     else:
         raise ("Unknown explainer type passed: %s!" % args.explainer)
     
-    scores = explainer(instructions)
+    explanations = explainer(instructions)
     # a list of dictionaries, each dictionary contains the explanation for a single instruction
-    print("Scores:", scores)
-    return scores
-
-def save_path(args):
-    save_dir = os.path.join(args.result_save_dir, f'explanations/{args.model_name}/{args.dataset}/{args.explainer}/seed_{args.seed}')
-    os.makedirs(save_dir, exist_ok=True)
-    filename = "explanations_"
-    filename += f"batch_{args.num_batch}_" if args.num_batch is not None else ""
-    filename += f"{args.dataset}_{args.model_name}_{args.explainer}_{args.seed}.pkl"
-    return os.path.join(save_dir, filename)
+    print("Explanations", explanations)
+    # save scores into args.results_dir
     
+    if save:
+        save_path = save_pkl_path(args, folder_name="explanations")
+        # Store in a dictionary
+        explanations_dict = {
+            "instructions": instructions,
+            "id": ids,
+            "explanations": explanations
+        }
+        with open(save_path, "wb") as f:
+            pkl.dump(explanations_dict, f)
+    
+    return explanations
+    
+
+
+if __name__ == "__main__":
+    parser, args = arg_parse()
+    compute_explanations(args)
